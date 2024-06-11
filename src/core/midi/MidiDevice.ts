3dca57295280b2f45651e00a4f82f1bdf25a8e98
Michalis Zamparas <mikezaby@gmail.com>
2024-06-11 11:39:46 +0300

    Implement core midi

---
+++ src/core/midi/MidiDevice.ts
@@ -0,0 +1,74 @@
+import MidiEvent from "./MidiEvent";
+
+export type TMidiPortState = "connected" | "disconnected";
+
+export interface MidiDeviceInterface {
+  id: string;
+  name: string;
+  state: TMidiPortState;
+}
+
+export interface IMidiInput extends MidiDeviceInterface {
+  onmidimessage: ((e: MidiEvent) => void) | null;
+}
+
+export type EventListerCallback = (event: MidiEvent) => void;
+
+export default class MidiDevice implements MidiDeviceInterface {
+  id: string;
+  name: string;
+  state: TMidiPortState;
+  eventListerCallbacks: EventListerCallback[] = [];
+
+  private _midi: IMidiInput;
+
+  constructor(midi: IMidiInput) {
+    this.id = midi.id;
+    this.name = midi.name || `Device ${midi.id}`;
+    this.state = midi.state;
+    this._midi = midi;
+
+    this.connect();
+  }
+
+  connect() {
+    this._midi.onmidimessage = (e: Event | MidiEvent) => {
+      const isMidiEvent =
+        e instanceof MIDIMessageEvent || e instanceof MidiEvent;
+
+      if (!isMidiEvent) return;
+
+      this.processEvent(e);
+    };
+  }
+
+  disconnect() {
+    this._midi.onmidimessage = null;
+  }
+
+  serialize() {
+    const { id, name, state } = this;
+
+    return { id, name, state };
+  }
+
+  addEventListener(callback: EventListerCallback) {
+    this.eventListerCallbacks.push(callback);
+  }
+
+  removeEventListener(callback: EventListerCallback) {
+    this.eventListerCallbacks = this.eventListerCallbacks.filter(
+      (c) => c !== callback,
+    );
+  }
+
+  private processEvent(e: MIDIMessageEvent | MidiEvent) {
+    const event: MidiEvent = e instanceof MidiEvent ? e : new MidiEvent(e);
+
+    switch (event.type) {
+      case "noteOn":
+      case "noteOff":
+        this.eventListerCallbacks.forEach((callback) => callback(event));
+    }
+  }
+}
---
+++ src/core/midi/MidiDeviceManager.ts
@@ -0,0 +1,75 @@
+import MidiDevice, { IMidiInput } from "./MidiDevice";
+
+export default class MidiDeviceManager {
+  devices: { [Key: string]: MidiDevice } = {};
+  private initialized = false;
+
+  constructor() {
+    this.initializeDevices()
+      .then(() => {
+        this.listenChanges();
+        this.initialized = true;
+      })
+      .catch(() => {});
+  }
+
+  find(id: string): MidiDevice | null {
+    const device = this.devices[id];
+
+    if (!device) return null;
+
+    return device;
+  }
+
+  onStateChange(callback: (device: MidiDevice) => void) {
+    navigator
+      .requestMIDIAccess()
+      .then((access: MIDIAccess) => {
+        access.onstatechange = (e) => {
+          const isMidiEvent = e instanceof MIDIConnectionEvent;
+
+          if (!isMidiEvent) return;
+          if (e.port instanceof MIDIOutput) return;
+
+          const input = e.port as unknown as IMidiInput;
+
+          const midi = new MidiDevice(input);
+
+          callback(midi);
+        };
+      })
+      .catch(() => {});
+  }
+
+  private listenChanges() {
+    this.onStateChange((device) => {
+      if (device.state === "disconnected") {
+        device.disconnect();
+        delete this.devices[device.id];
+      } else {
+        this.devices[device.id] = device;
+      }
+    });
+  }
+
+  private async initializeDevices() {
+    if (this.initialized) return Object.values(this.devices);
+
+    (await this.inputs()).forEach((input) => {
+      if (this.devices[input.id]) return;
+
+      this.devices[input.id] = new MidiDevice(input as IMidiInput);
+    });
+
+    return Object.values(this.devices);
+  }
+
+  private async inputs() {
+    const inputs: MIDIInput[] = [];
+
+    const access = await navigator.requestMIDIAccess();
+    access.inputs.forEach((input) => inputs.push(input));
+
+    return inputs;
+  }
+}
