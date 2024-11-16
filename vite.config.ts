422eef77d3d7e913551436424827dff1f9aaeb3a
Michalis Zamparas <mikezaby@gmail.com>
2024-11-16 19:08:05 +0200

    Add the ability to import from src like @/

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"), // Adjust './src' to your project root folder
+    },
+  },
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
