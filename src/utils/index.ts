ebd7af7332f7f46dcde8904dd3f9ca5d773dfdf2
Michalis Zabaras <mikezaby@gmail.com>
2025-02-21 10:27:48 +0200

    Implement function that convert brower time to context time

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -16,3 +16,8 @@ export function uuidv4() {
 export function now() {
   return Engine.current.context.currentTime;
 }
+
+export function browserToContextTime(time: number): number {
+  const differenceBetweenClocks = performance.now() / 1000 - now();
+  return time / 1000 - differenceBetweenClocks;
+}
