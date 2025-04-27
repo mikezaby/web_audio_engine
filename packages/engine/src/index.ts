59a6b1931ddca8407918fdc623835a600557ce6e
Michalis Zabaras <mikezaby@gmail.com>
2025-04-27 22:16:29 +0300

    Expose IRoute type

--- packages/engine/src/index.ts
+++ packages/engine/src/index.ts
@@ -1,4 +1,7 @@
 export { Engine } from "./Engine";
+export type { IRoute } from "./core";
+
 export { ModuleType } from "./modules";
 export type { IOscillator, IVolume, IMaster } from "./modules";
+
 export { TransportState } from "@/core/Timing";
