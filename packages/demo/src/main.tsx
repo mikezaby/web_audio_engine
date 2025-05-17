dcd55039632ca2dc7e01c91b13468f604ccdca9c
Michalis Zabaras <mikezaby@gmail.com>
2025-05-17 17:20:41 +0300

    Fix formating on demo

--- packages/demo/src/main.tsx
+++ packages/demo/src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./style.css";
 import App from "./App";
+import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
